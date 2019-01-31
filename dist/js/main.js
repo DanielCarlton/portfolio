679670e7ec276ff0fa041ceba654a2a459695de7
Daniel Carlton <daniel_carlton91@hotmail.com>
2019-01-31 20:18:55 +1000

    Menu button, JS and rotate

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// Select DOM items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set intial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(navItem => navItem.classList.add('show'));
+
+    // Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(navItem => navItem.classList.remove('show'));
+
+    // Set menu state
+    showMenu = false;
+  }
+}
